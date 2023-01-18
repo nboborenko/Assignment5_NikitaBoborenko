4d1edddd5d46868bbfb087a7b2c2749d23190ef9
poros <poros@DESKTOP-VS58TJL>
2023-01-18 22:48:25 +0200

    Добавьте файлы проекта.

---
+++ Assignment5_NikitaBoborenko/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Assignment5_NikitaBoborenko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guesses_label = new System.Windows.Forms.Label();
+            this.guess_button = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // guesses_label
+            // 
+            this.guesses_label.AutoSize = true;
+            this.guesses_label.Location = new System.Drawing.Point(211, 156);
+            this.guesses_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.guesses_label.Name = "guesses_label";
+            this.guesses_label.Size = new System.Drawing.Size(98, 13);
+            this.guesses_label.TabIndex = 2;
+            this.guesses_label.Text = "Number of guesses";
+            this.guesses_label.Click += new System.EventHandler(this.guesses_label_Click);
+            // 
+            // guess_button
+            // 
+            this.guess_button.Location = new System.Drawing.Point(294, 171);
+            this.guess_button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.guess_button.Name = "guess_button";
+            this.guess_button.Size = new System.Drawing.Size(56, 19);
+            this.guess_button.TabIndex = 4;
+            this.guess_button.Text = "Guess";
+            this.guess_button.UseVisualStyleBackColor = true;
+            this.guess_button.Click += new System.EventHandler(this.guess_button_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(214, 171);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(76, 20);
+            this.inputTextBox.TabIndex = 5;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.guess_button);
+            this.Controls.Add(this.guesses_label);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label guesses_label;
+        private System.Windows.Forms.Button guess_button;
+        private System.Windows.Forms.TextBox inputTextBox;
+    }
+}
+
---
+++ Assignment5_NikitaBoborenko/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment5_NikitaBoborenko
+{
+    public partial class Form1 : Form
+    {
+        int random_number;
+        int number_of_guesses;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int generate_random_number()
+        { 
+            Random rnd = new Random();
+            int random_number = rnd.Next(0, 101);
+            return random_number;
+        }
+
+        public void start_new_game()
+        {
+            number_of_guesses = 0;
+            random_number = generate_random_number();
+            guesses_label.Text = "Number of guesses: " + Convert.ToString(number_of_guesses);
+        }
+        public void failed_attempt()
+        {
+            number_of_guesses++;
+            guesses_label.Text = "Number of guesses: " + Convert.ToString(number_of_guesses);
+        }
+        private void guess_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(inputTextBox.Text) == random_number)
+                {
+                    MessageBox.Show("You guessed number "+ Convert.ToString(random_number) +" in "+ Convert.ToString(number_of_guesses) + " attempts");
+                    start_new_game();
+                }
+                else if (Convert.ToInt32(inputTextBox.Text) < random_number)
+                {
+                    failed_attempt();
+                    MessageBox.Show("Guess is less than random number");
+                }
+                else if (Convert.ToInt32(inputTextBox.Text) > random_number)
+                {
+                    failed_attempt();
+                    MessageBox.Show("Guess is more than random number");
+                }
+                
+            }
+            catch
+            {
+                MessageBox.Show("Input is empty");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            number_of_guesses = 0;
+            random_number=generate_random_number();
+            guesses_label.Text= "Number of guesses: "+Convert.ToString(number_of_guesses);
+
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(inputTextBox.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Only numbers can be entered");
+                inputTextBox.Text = inputTextBox.Text.Remove(inputTextBox.Text.Length - 1);
+            }
+        }
+
+        private void guesses_label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
